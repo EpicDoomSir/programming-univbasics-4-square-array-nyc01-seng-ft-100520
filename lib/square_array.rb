b8e0b3dbab9899e25666d2a53603c6877eee1044
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-22 16:15:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0
   while counter < array.size do
-    new_array << Math.sqrt(array[counter])
+    new_array << Math.sqr(array[counter])
     counter += 1
   end
   new_array
